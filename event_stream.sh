a50c10a869115ca437ae4b6a19a36f8ce2e1c379
ITestTheCodes <130445058+ITestTheCodes@users.noreply.github.com>
2023-04-17 23:30:29 +0300

    Update event_stream.sh

--- event_stream.sh
+++ event_stream.sh
@@ -1,30 +1,31 @@
 # Installation
 
-$ sha512sum kafka_2.13–3.4.0.tgz
+sha512sum kafka_2.13–3.4.0.tgz
 
-$ tar -xzf kafka_2.13–3.4.0.tgz
-$ cd kafka_2.13–3.4.0
+tar -xzf kafka_2.13–3.4.0.tgz
+cd kafka_2.13–3.4.0
 
 # Start the servers
 
-$ bin/zookeeper-server-start.sh config/zookeeper.properties
+bin/zookeeper-server-start.sh config/zookeeper.properties
 
-$ bin/kafka-server-start.sh config/server.properties
+bin/kafka-server-start.sh config/server.properties
 
 #Create topics
 
-$ bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor --partitions 1 --topic test
+bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor --partitions 1 --topic test
 
-$ bin/kafka-topics.sh --list --bootstrap-server localhost:9092
+bin/kafka-topics.sh --list --bootstrap-server localhost:9092
 
-Start producers
+# Start producers
 
-$ bin/kafka-console-producer.sh --bootstrap-server localhost:9092 --topic test
+bin/kafka-console-producer.sh --bootstrap-server localhost:9092 --topic test
 
-Start consumers
+# Start consumers
 
-$ bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic test --from-beginning
+bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic test --from-beginning
 
-Clean up resources
-$ bin/kafka-server-stop.sh
-$ bin/zookeeper-server-stop.sh
+# Clean up resources
+
+bin/kafka-server-stop.sh
+bin/zookeeper-server-stop.sh
