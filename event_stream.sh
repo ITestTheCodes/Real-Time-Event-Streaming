6108729d0b9c60e22f4ca18aa9220be4db316b41
ITestTheCodes <130445058+ITestTheCodes@users.noreply.github.com>
2023-04-17 23:40:44 +0300

    Update event_stream.sh

--- event_stream.sh
+++ event_stream.sh
@@ -7,25 +7,25 @@ cd kafka_2.13–3.4.0
 
 # Start the servers
 
-bin/zookeeper-server-start.sh config/zookeeper.properties
+.bin/zookeeper-server-start.sh config/zookeeper.properties
 
-bin/kafka-server-start.sh config/server.properties
+.bin/kafka-server-start.sh config/server.properties
 
 #Create topics
 
-bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor --partitions 1 --topic test
+.bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor --partitions 1 --topic test
 
-bin/kafka-topics.sh --list --bootstrap-server localhost:9092
+.bin/kafka-topics.sh --list --bootstrap-server localhost:9092
 
 # Start producers
 
-bin/kafka-console-producer.sh --bootstrap-server localhost:9092 --topic test
+.bin/kafka-console-producer.sh --bootstrap-server localhost:9092 --topic test
 
 # Start consumers
 
-bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic test --from-beginning
+.bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic test --from-beginning
 
 # Clean up resources
 
-bin/kafka-server-stop.sh
-bin/zookeeper-server-stop.sh
+.bin/kafka-server-stop.sh
+.bin/zookeeper-server-stop.sh
